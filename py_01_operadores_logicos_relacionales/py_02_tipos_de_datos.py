8e0d2635f2d6a2e141ccb10b76740b7d9fdc68a0
Zolyn Linux <152818486+zolynux@users.noreply.github.com>
2024-07-31 03:34:49 +0000

    subiendo

---
+++ py_01_operadores_logicos_relacionales/py_02_tipos_de_datos.py
@@ -0,0 +1,58 @@
+numero = 30
+flotante = 34.23
+cadena = "Hola mundo"
+booleano = True
+lista = [1, 2, 3, 4, 5]
+tupla = (1, 2, 3, 4)
+diccionario = {"nombre": "Zolynux", "edad": 30}
+
+print(numero)
+print(flotante)
+print(cadena)
+print(booleano)
+print(lista)
+print(tupla)
+print(diccionario)
+
+# Se muestra los tipos
+print(type(numero))
+print(type(flotante))
+print(type(cadena))
+print(type(booleano))
+print(type(lista))
+print(type(tupla))
+print(type(diccionario))
+
+# Operadores suma, resta, multiplicacion, division, modulo, potencia
+
+n1 = 120
+n2 = 30
+
+# Suma
+suma = n1 + n2
+print(suma)
+
+# Resta
+resta = n1 - n2
+print(resta)
+
+# Multiplicacion
+multiplicacion = n1 * n2
+print(multiplicacion)
+
+# Division
+division = n1 / n2
+print(division)
+
+# Modulo
+modulo = n1 % n2
+print(modulo)
+
+# Potencia
+potencia = n1 ** n2
+print(potencia)
+
+
+# El valor de tipos de datos dinamicos en variable
+dato = 30
+print(dato)
